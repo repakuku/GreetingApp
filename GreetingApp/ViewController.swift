afffe0a935dc0b9ff077a5fee82df5c268a2961e
Alexey Turulin <repakuku@icloud.com>
2023-09-22 16:26:01 -0400

    working with VIPER

--- GreetingApp/ViewController.swift
+++ GreetingApp/ViewController.swift
@@ -11,14 +11,16 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var greetingLabel: UILabel!
     
+    private var person: Person!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        person = Person(name: "Tim", surname: "Cook")
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
-        
+        greetingLabel.text = "Hello, \(person.name) \(person.surname)!"
     }
     
 }
